3224893765fd0b4c9bc1dfe84dd26e9fed8273dc
James Moore <jamesm0025@gmail.com>
2019-03-04 21:31:26 -0500

    changed solution to use associtive array replace each letter with its complimnt using strtr replace pairs

--- php/rna-transcription/rna-transcription.php
+++ php/rna-transcription/rna-transcription.php
@@ -1,21 +1,7 @@
 <?php
 
 function toRna(string $dnaStrand) : string {
-    for ($letter = 0 ; $letter < strlen($dnaStrand); $letter++) {
-        switch ($dnaStrand[$letter]) {
-            case 'G':
-                $dnaStrand[$letter] = 'C';
-                break;
-            case 'C':
-                $dnaStrand[$letter] = 'G';
-                break;
-            case 'T':
-                $dnaStrand[$letter] = 'A';
-                break;
-            case 'A':
-                $dnaStrand[$letter] = 'U';
-                break;
-        }
-    }
-    return $dnaStrand;
+    $compliments = array("G" => "C", "C" => "G", "T" => "A", "A" => "U");
+    $rnaStrand = strtr($dnaStrand,$compliments);
+    return $rnaStrand;
 }
