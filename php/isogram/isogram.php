239e68697032e354fe5921d30cd1ea8701235df1
James Moore <jamesm0025@gmail.com>
2019-03-14 13:16:52 -0400

    isogram php solution iteration 1

--- php/isogram/isogram.php
+++ php/isogram/isogram.php
@@ -1,18 +1,31 @@
 <?php
 
-function isIsogram(string $input) : bool {
+function isIsogram(string $input) : bool
+{
+    $input = removeSpacesAndHyphens($input);
+    $input = mb_strtolower($input);
+    $characters = preg_split('//u', $input, -1, PREG_SPLIT_NO_EMPTY);
+    $uniqueCharacters = [];
 
-    // Remove the cases with hyphens and make it lowercase
-    $input = str_replace([' ', '-'], '', $input);
-    $input = strtolower($input);
+    foreach ($characters as $character ) {
+        if(!in_array($character, $uniqueCharacters)) {
+            array_push($uniqueCharacters, $character);
+        }
+    }
 
-    $uniqueCharacters = count_chars($input, 3);
+    return isEqualLength($characters, $uniqueCharacters);
+}
 
-    var_dump($input);
-    var_dump($uniqueCharacters);
+function removeSpacesAndHyphens(string $input) : string
+{
+    return str_replace([' ', '-'], '', $input);
+}
 
-    // length isnt working for the german letters .. try an arr
-    if(strlen($input) == strlen($uniqueCharacters)) {
+function isEqualLength($characters, $uniqueCharacters) : bool
+{
+    if(count($characters) === count($uniqueCharacters)) {
         return true;
-    } else return false;
+    } else {
+        return false;
+    }
 }
