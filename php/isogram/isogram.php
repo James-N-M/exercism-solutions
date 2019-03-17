c4c3ea89fb757b14e74198703005e78b602e88f6
James Moore <jamesm0025@gmail.com>
2019-03-16 21:32:04 -0400

    updated isogram solution removed nested loops

--- php/isogram/isogram.php
+++ php/isogram/isogram.php
@@ -3,15 +3,12 @@
 function isIsogram(string $input) : bool
 {
     $input = removeSpacesAndHyphens($input);
-    $input = mb_strtolower($input);
-    $characters = preg_split('//u', $input, -1, PREG_SPLIT_NO_EMPTY);
-    $uniqueCharacters = [];
 
-    foreach ($characters as $character ) {
-        if(!in_array($character, $uniqueCharacters)) {
-            array_push($uniqueCharacters, $character);
-        }
-    }
+    $input = mb_strtolower($input);
+
+    $characters = preg_split('//u', $input, -1, PREG_SPLIT_NO_EMPTY);
+
+    $uniqueCharacters = array_unique($characters);
 
     return isEqualLength($characters, $uniqueCharacters);
 }
@@ -23,9 +20,5 @@ function removeSpacesAndHyphens(string $input) : string
 
 function isEqualLength($characters, $uniqueCharacters) : bool
 {
-    if(count($characters) === count($uniqueCharacters)) {
-        return true;
-    } else {
-        return false;
-    }
+    return (count($characters) === count($uniqueCharacters));
 }
