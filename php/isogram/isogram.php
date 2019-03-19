8bc646e0f6b3060e2bb1b7d65d51b9d4ce228c14
James Moore <jamesm0025@gmail.com>
2019-03-19 14:51:39 -0400

    match any character except german letters

--- php/isogram/isogram.php
+++ php/isogram/isogram.php
@@ -2,30 +2,9 @@
 
 function isIsogram(string $input) : bool
 {
-    $input = removeSpacesAndHyphens($input);
-    $input = mb_strtolower($input);
-    $characters = preg_split('//u', $input, -1, PREG_SPLIT_NO_EMPTY);
-    $uniqueCharacters = [];
+    preg_match_all('/\p{L}/', mb_strtolower($input), $characters);
 
-    foreach ($characters as $character ) {
-        if(!in_array($character, $uniqueCharacters)) {
-            array_push($uniqueCharacters, $character);
-        }
-    }
-
-    return isEqualLength($characters, $uniqueCharacters);
-}
-
-function removeSpacesAndHyphens(string $input) : string
-{
-    return str_replace([' ', '-'], '', $input);
-}
-
-function isEqualLength($characters, $uniqueCharacters) : bool
-{
-    if(count($characters) === count($uniqueCharacters)) {
-        return true;
-    } else {
-        return false;
-    }
+    $uniqueCharacters = array_unique($characters[0]);
+    print_r($characters);
+    return (count($characters[0]) === count($uniqueCharacters));
 }
