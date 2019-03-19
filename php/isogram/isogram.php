b6e5058a6c7efd7ce6e834e0515951c80f68157b
James Moore <jamesm0025@gmail.com>
2019-03-19 15:04:47 -0400

    refactored to use regex and passes all except german letters

--- php/isogram/isogram.php
+++ php/isogram/isogram.php
@@ -5,6 +5,6 @@ function isIsogram(string $input) : bool
     preg_match_all('/\p{L}/', mb_strtolower($input), $characters);
 
     $uniqueCharacters = array_unique($characters[0]);
-    print_r($characters);
+
     return (count($characters[0]) === count($uniqueCharacters));
 }
