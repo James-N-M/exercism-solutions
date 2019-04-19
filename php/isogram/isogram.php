ca11ebc4ccbe2f11a1e87a702f77ceb9000ab95e
James Moore <jamesm0025@gmail.com>
2019-04-19 16:19:56 -0400

    isogram changes

--- php/isogram/isogram.php
+++ php/isogram/isogram.php
@@ -2,23 +2,9 @@
 
 function isIsogram(string $input) : bool
 {
-    $input = removeSpacesAndHyphens($input);
-
-    $input = mb_strtolower($input);
-
-    $characters = preg_split('//u', $input, -1, PREG_SPLIT_NO_EMPTY);
+    preg_match_all('//u',mb_strtolower($input), $characters);
 
     $uniqueCharacters = array_unique($characters);
 
-    return isEqualLength($characters, $uniqueCharacters);
-}
-
-function removeSpacesAndHyphens(string $input) : string
-{
-    return str_replace([' ', '-'], '', $input);
-}
-
-function isEqualLength($characters, $uniqueCharacters) : bool
-{
     return (count($characters) === count($uniqueCharacters));
 }
