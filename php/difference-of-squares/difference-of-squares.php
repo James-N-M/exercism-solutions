64cca2aa6aef68503ce54a10def47938843af917
James Moore <jamesm0025@gmail.com>
2019-04-27 12:01:40 -0400

    difference of squares solution

---
+++ php/difference-of-squares/difference-of-squares.php
@@ -0,0 +1,22 @@
+<?php
+define('POWER_OF_TWO', 2);
+
+function difference(int $number) : int
+{
+    return squareOfSum($number) - sumOfSquares($number);
+}
+
+function squareOfSum(int $number) : int
+{
+    return squared(array_sum(range(1,$number)));
+}
+
+function sumOfSquares(int $number) : int
+{
+    return array_sum(array_map("squared", range(1, $number)));
+}
+
+function squared(int $number) : int
+{
+    return pow($number, POWER_OF_TWO);
+}
