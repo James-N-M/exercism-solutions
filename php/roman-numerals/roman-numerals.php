8b66dd0e305d142a5cd57dc969f83ec99dc78f7a
James Moore <jamesm0025@gmail.com>
2019-12-23 17:27:46 -0500

    finished roman numerals challenge

--- php/roman-numerals/roman-numerals.php
+++ php/roman-numerals/roman-numerals.php
@@ -1,1 +1,35 @@
 <?php
+
+const NUMERALS = [
+    'M' => 1000,
+    'CM' => 900,
+    'D' => 500,
+    'CD' => 400,
+    'C' => 100,
+    'XC' => 90,
+    'L' => 50,
+    'XL' => 40,
+    'X' => 10,
+    'IX' => 9,
+    'VIII' => 8,
+    'VII' => 7,
+    'VI' => 6,
+    'V' => 5,
+    'IV' => 4,
+    'III' => 3,
+    'II' => 2,
+    'I' => 1,
+];
+
+function toRoman($number): string {
+
+    $result = "";
+
+    foreach (NUMERALS as $numeral => $value) {
+        for (; $number >= $value; $number -= $value) {
+            $result .= $numeral;
+        }
+    }
+
+    return $result;
+}
