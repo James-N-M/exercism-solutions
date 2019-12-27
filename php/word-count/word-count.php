be47811746b8d049ce0b53d602c475c634051cfe
James Moore <jamesm0025@gmail.com>
2019-12-26 22:55:54 -0500

    updated formated

--- php/word-count/word-count.php
+++ php/word-count/word-count.php
@@ -1,7 +1,12 @@
 <?php
 
 function wordCount($words) : array {
-     return array_count_values(
-         preg_split('/[\s,:!&@$%^]+/u', strtolower($words), -1, PREG_SPLIT_NO_EMPTY|PREG_SPLIT_DELIM_CAPTURE)
-     );
+    return array_count_values(
+        preg_split(
+            '/[\s,:!&@$%^]+/u',
+            strtolower($words),
+            -1,
+            PREG_SPLIT_NO_EMPTY|PREG_SPLIT_DELIM_CAPTURE
+        )
+    );
 }
